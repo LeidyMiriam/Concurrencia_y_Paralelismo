3ac0ae9d2d1161cc93a01a92a46c7e8850c02930
LeidyMiriam <al075853@uacam.mx>
2026-09-23 15:55:35 -0600

    :tada: feat: Entrega practica03

--- Practica03/Montecarlo_Semaforos.c
+++ Practica03/Montecarlo_Semaforos.c
@@ -6,7 +6,7 @@
 
 
 #define TOTAL_PUNTOS 100000000
-#define NUM_HILOS 16
+#define NUM_HILOS 2
 
 int puntosDentro = 0;
 
@@ -35,43 +35,16 @@ void *monteCarlo(void *arg)
 int main(void){
 clock_t inicio = clock();
 
-pthread_t hilo1, hilo2, hilo3, hilo4, hilo5, hilo6, hilo7, hilo8, hilo9, hilo10, hilo11, hilo12, hilo13, hilo14, hilo15, hilo16;
+pthread_t hilo1, hilo2;
 
     sem_init(&semaforo, 0, 1);
 
     pthread_create(&hilo1, NULL, monteCarlo, NULL);
     pthread_create(&hilo2, NULL, monteCarlo, NULL);
-    pthread_create(&hilo3, NULL, monteCarlo, NULL);
-    pthread_create(&hilo4, NULL, monteCarlo, NULL);
-    pthread_create(&hilo5, NULL, monteCarlo, NULL);
-    pthread_create(&hilo6, NULL, monteCarlo, NULL);
-    pthread_create(&hilo7, NULL, monteCarlo, NULL);
-    pthread_create(&hilo8, NULL, monteCarlo, NULL);
-    pthread_create(&hilo9, NULL, monteCarlo, NULL);
-    pthread_create(&hilo10, NULL, monteCarlo, NULL);
-    pthread_create(&hilo11, NULL, monteCarlo, NULL);
-    pthread_create(&hilo12, NULL, monteCarlo, NULL);
-    pthread_create(&hilo13, NULL, monteCarlo, NULL);
-    pthread_create(&hilo14, NULL, monteCarlo, NULL);
-    pthread_create(&hilo15, NULL, monteCarlo, NULL);
-    pthread_create(&hilo16, NULL, monteCarlo, NULL);
+
 
     pthread_join(hilo1, NULL);
     pthread_join(hilo2, NULL);
-    pthread_join(hilo3, NULL);
-    pthread_join(hilo4, NULL);
-    pthread_join(hilo5, NULL);
-    pthread_join(hilo6, NULL);
-    pthread_join(hilo7, NULL);
-    pthread_join(hilo8, NULL);
-    pthread_join(hilo9, NULL);
-    pthread_join(hilo10, NULL);
-    pthread_join(hilo11, NULL);
-    pthread_join(hilo12, NULL);
-    pthread_join(hilo13, NULL);
-    pthread_join(hilo14, NULL);
-    pthread_join(hilo15, NULL);
-    pthread_join(hilo16, NULL);
 
     double pi = 4.0 * puntosDentro / TOTAL_PUNTOS;
 
